0874a0b06e9fd70d610c609d8270771293b9d00f
seil0224 <86836075+seil0224@users.noreply.github.com>
2021-07-07 02:58:16 +0900

    Update answer1.php

--- labyrinth/answer1.php
+++ labyrinth/answer1.php
@@ -1,5 +1,5 @@
 <? php
-  $passcode = $_GET["passcode"];
+  $passcode = $_GET['passcode'];
   if($passcode == "1234"){
     echo "<script> location = https://seil0224.github.io/labyrinth/penetrate.txt </script>";
   }

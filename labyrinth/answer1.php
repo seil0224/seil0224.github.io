26db59e5b95f221a2caa6d9b01be21908b905805
seil0224 <86836075+seil0224@users.noreply.github.com>
2021-07-07 03:06:49 +0900

    Delete answer1.php

--- labyrinth/answer1.php
+++
@@ -1,9 +0,0 @@
-<? php
-  $passcode = $_GET['passcode'];
-  if($passcode == "1234"){
-    echo "<script> location = https://seil0224.github.io/labyrinth/penetrate.txt </script>";
-  }
-  else{
-    echo "<script> location = https://seil0224.github.io/labyrinth/penetrate.md </script>";
-  }
-?>
